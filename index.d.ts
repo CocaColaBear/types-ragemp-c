961079138b295927db2dd9b0153147ac64fba706
gatno <danieltaucher@gmail.com>
2018-03-13 09:31:23 +0100

    Fixed setHeadOverlay Parameters

--- index.d.ts
+++ index.d.ts
@@ -752,7 +752,7 @@ interface PlayerMp extends EntityMp {
 	setHasReserveParachute(): void;
 	setHeadBlendData(shapeFirstID: number, shapeSecondID: number, shapeThirdID: number, skinFirstID: number,
 		skinSecondID: number, skinThirdID: number, shapeMix: number, skinMix: number, thirdMix: number, isParent: boolean): void;
-	setHeadOverlay(overlayID: number, index: number, opacity: number): void;
+	setHeadOverlay(overlayID: number, index: number, opacity: number, firstColor: number, secondColor: number): void;
 	setHeadOverlayColor(overlayID: number, colorType: number, colorID: number, secondColorID: number): void;
 	setHealthRechargeMultiplier(regenRate: number): void;
 	setHearingRange(value: number): void;
