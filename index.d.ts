6087a894e20b86ed1c1f077aed50f5aa549c3692
CocaColaBear <myandroidcool@gmail.com>
2020-04-19 21:50:05 +0300

    Removed setAlpha(alpha: number) from EntityMp Version bump

--- index.d.ts
+++ index.d.ts
@@ -298,7 +298,6 @@ interface EntityMp {
 		p6: number): boolean;
 	processAttachments(): void;
 	resetAlpha(): void;
-	setAlpha(alpha: number) : void;
 	setAlpha(alphaLevel: number, skin: boolean): void;
 	setAlwaysPrerender(toggle: boolean): void;
 	setAnimCurrentTime(animDict: string, animName: string, time: number): void;
