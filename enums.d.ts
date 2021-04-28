a0437680cbeb89e1b840f979426b1bab94d2ea48
Guy Incognito <lJohhnyl@outlook.com>
2021-04-28 17:40:55 +0300

    Add Props
    Signed-off-by: Guy Incognito <lJohhnyl@outlook.com>

--- enums.d.ts
+++ enums.d.ts
@@ -4532,6 +4532,39 @@ declare namespace RageEnums {
 		PICKUP_WEAPON_WRENCH = 0xE5121369,
 	}
 
+	const enum Props {
+		/**
+		 * Male: https://wiki.rage.mp/index.php?title=Male_Hats
+		 *
+		 * Female: https://wiki.rage.mp/index.php?title=Female_Hats
+		 */
+		HATS = 0,
+		/**
+		 * Male: https://wiki.rage.mp/index.php?title=Male_Glasses
+		 *
+		 * Female: https://wiki.rage.mp/index.php?title=Female_Glasses
+		 */
+		GLASSES = 1,
+		/**
+		 * Male: https://wiki.rage.mp/index.php?title=Male_Ears
+		 *
+		 * Female: https://wiki.rage.mp/index.php?title=Female_Ears
+		 */
+		EARS = 2,
+		/**
+		 * Male: https://wiki.rage.mp/index.php?title=Male_Watches
+		 *
+		 * Female: https://wiki.rage.mp/index.php?title=Female_Watches
+		 */
+		WATCHES = 6,
+		/**
+		 * Male: https://wiki.rage.mp/index.php?title=Male_Bracelets
+		 *
+		 * Female: https://wiki.rage.mp/index.php?title=Female_Bracelets
+		 */
+		BRACELETS = 7
+	}
+
 	const enum ScreenshotType {
 		JPG = 0,
 		PNG = 1,
