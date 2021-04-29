4ee761e0a50786a78bb2b4858d8a6aa79ee4a863
Guy Incognito <lJohhnyl@outlook.com>
2021-04-29 18:24:42 +0300

    v2: Add Vehicle Mods
    Signed-off-by: Guy Incognito <lJohhnyl@outlook.com>

--- enums.d.ts
+++ enums.d.ts
@@ -2,49 +2,6 @@ declare namespace RageEnums {
 	const enum VehicleColorsHex {
 	}
 
-	const enum VehicleMods {
-		SPOILERS = 0,
-		FRONT_BUMPER = 1,
-		REAR_BUMPER = 2,
-		SIDE_SKIRT = 3,
-		EXHAUST = 4,
-		FRAME = 5,
-		GRILLE = 6,
-		HOOD = 7,
-		FENDER = 8,
-		RIGHT_FENDER = 9,
-		ROOF = 10,
-		ENGINE = 11,
-		BRAKES = 12,
-		TRANSMISSION = 13,
-		/** 0 to 51 */
-		HORNS = 14,
-		SUSPENSION = 15,
-		ARMOR = 16,
-		TURBO = 18,
-		XENON = 22,
-		FRONT_WHEELS = 23,
-		UTIL_SHADOW_SILVER = 20,
-		/** Only for Motorcycles */
-		BACK_WHEELS = 24,
-		PLATE_HOLDERS = 25,
-		TRIM_DESIGN = 27,
-		ORNAMENTS = 28,
-		DIAL_DESIGN = 30,
-		STEERING_WHEEL = 33,
-		SHIFT_LEVER = 34,
-		PLAQUES = 35,
-		HYDRAULICS = 38,
-		BOOST = 40,
-		WINDOW_TINT = 55,
-		LIVERY = 48,
-		PLATE = 62,
-		/** 0 to 74 */
-		COLOUR_1 = 66,
-		/** 0 to 74 */
-		COLOUR_2 = 67,
-	}
-
 	/**
 	 * Wikipedia: [https://wiki.rage.mp/index.php?title=Weather](https://wiki.rage.mp/index.php?title=Weather)
 	 */
