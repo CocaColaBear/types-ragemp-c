6873cf1483bc4d3d959674a0810c9da61a6943f6
Guy Incognito <lJohhnyl@outlook.com>
2021-04-29 17:46:04 +0300

    v2: Add Hud Components
    Signed-off-by: Guy Incognito <lJohhnyl@outlook.com>

--- enums.d.ts
+++ enums.d.ts
@@ -1,28 +1,4 @@
 declare namespace RageEnums {
-	const enum HudComponent {
-		HUD = 0,
-		WANTED_STARS = 1,
-		WEAPON_ICON = 2,
-		CASH = 3,
-		MP_CASH = 4,
-		MP_MESSAGE = 5,
-		VEHICLE_NAME = 6,
-		AREA_NAME = 7,
-		VEHICLE_CLASS = 8,
-		STREET_NAME = 9,
-		HELP_TEXT = 10,
-		FLOATING_HELP_TEXT_1 = 11,
-		FLOATING_HELP_TEXT_2 = 12,
-		CASH_CHANGE = 13,
-		RETICLE = 14,
-		SUBTITLE_TEXT = 15,
-		RADIO_STATIONS = 16,
-		SAVING_GAME = 17,
-		GAME_STREAM = 18,
-		WEAPON_WHEEL = 19,
-		WEAPON_WHEEL_STATS = 20
-	}
-
 	const enum InputGroup {
 		INPUTGROUP_MOVE = 0,
 		INPUTGROUP_LOOK = 1,
--- enums/hud.d.ts
+++ enums/hud.d.ts
@@ -450,4 +450,33 @@ declare namespace RageEnums.Hud {
 		/** ![002828](https://htmlcolors.com/color-image/002828.png) */
 		TECH_GREEN_VERY_DARK = "HUD_COLOUR_TECH_GREEN_VERY_DARK",
 	}
+
+	/** [https://wiki.rage.mp/index.php?title=HUD_Components](https://wiki.rage.mp/index.php?title=HUD_Components) */
+	const enum Component {
+		HUD = 0,
+		WANTED_STARS = 1,
+		WEAPON_ICON = 2,
+		CASH = 3,
+		MP_CASH = 4,
+		MP_MESSAGE = 5,
+		VEHICLE_NAME = 6,
+		AREA_NAME = 7,
+		VEHICLE_CLASS = 8,
+		STREET_NAME = 9,
+		HELP_TEXT = 10,
+		FLOATING_HELP_TEXT_1 = 11,
+		FLOATING_HELP_TEXT_2 = 12,
+		CASH_CHANGE = 13,
+		RETICLE = 14,
+		SUBTITLE_TEXT = 15,
+		RADIO_STATIONS = 16,
+		SAVING_GAME = 17,
+		GAME_STREAM = 18,
+		WEAPON_WHEEL = 19,
+		WEAPON_WHEEL_STATS = 20
+	}
+}
+
+declare namespace RageEnums {
+	export import HudComponent = RageEnums.Hud.Component;
 }
