e81802833796725f7d995363ea8391ea055bba52
Guy Incognito <lJohhnyl@outlook.com>
2021-06-12 21:11:04 +0300

    v2: Update description createObject
    Signed-off-by: Guy Incognito <lJohhnyl@outlook.com>

--- index.d.ts
+++ index.d.ts
@@ -2421,6 +2421,23 @@ interface GameObjectMp {
 	createAmbientPickup(pickupHash: Hash, posX: number, posY: number, posZ: number, p4: number, value: number,
 		modelHash: Hash, p7: boolean, p8: boolean): PickupMp;
 	createMoneyPickups(x: number, y: number, z: number, value: number, amount: number, model: Hash): void;
+	/**
+	 * Creates an object (prop) with the specified model centered at the specified position.
+	 This object will initially be owned by the creating script as a mission entity, and the model should be loaded already (e.g. using REQUEST_MODEL).
+	 *
+	 * @param {Hash} modelHash - The model to spawn.
+	 *
+	 * @param {number} x - Spawn coordinate X component.
+	 * @param {number} y - Spawn coordinate Y component.
+	 * @param {number} z - Spawn coordinate Z component.
+	 * @param {boolean} networkHandle - Whether to create a network object for the object. If false, the object exists only locally.
+	 * @param {boolean} createHandle - Whether to register the object as pinned to the script host in the R\* network model.
+	 * @param {boolean} dynamic - False to create a door archetype (archetype flag bit 26 set) as a door. Required to be set to true to create door models in network mode.
+	 *
+	 * @return {Handle} - A script handle (fwScriptGuid index) for the object, or `0` if the object failed to be created.
+	 *
+	 * {@link https://wiki.rage.mp/index.php?title=Object::createObject|Object::createObject}
+	 */
 	createObject(modelHash: Hash, x: number, y: number, z: number, networkHandle: boolean,
 		createHandle: boolean,dynamic: boolean): Handle;
 	createObjectNoOffset(modelHash: Hash, x: number, y: number, z: number, networkHandle: boolean,
