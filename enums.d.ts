2f3e2017df916741d3eac91f339b460586ba0417
Guy Incognito <lJohhnyl@outlook.com>
2021-06-12 21:00:24 +0300

    v2: Add Scripts
    Signed-off-by: Guy Incognito <lJohhnyl@outlook.com>

--- enums.d.ts
+++ enums.d.ts
@@ -13,6 +13,7 @@
 /// <reference path="enums/pickups.d.ts" />
 /// <reference path="enums/props.d.ts" />
 /// <reference path="enums/screenshot.d.ts" />
+/// <reference path="enums/scripts.d.ts" />
 /// <reference path="enums/vehicle.d.ts" />
 /// <reference path="enums/weapons.d.ts" />
 /// <reference path="enums/weather.d.ts" />
